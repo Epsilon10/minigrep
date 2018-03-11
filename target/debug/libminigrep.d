a0ba53d3aa28ebdd91f4735786e4e1ea0e67af70
Epsilon10 <benjamin.oommen1@gmail.com>
2018-03-11 16:50:16 +0530

    removed target

--- target/debug/libminigrep.d
+++
@@ -1,1 +0,0 @@
-/Users/moommen/Documents/rust_projects/minigrep/target/debug/libminigrep.rlib: /Users/moommen/Documents/rust_projects/minigrep/src/lib.rs
--- target/debug/minigrep.d
+++
@@ -1,1 +0,0 @@
-/Users/moommen/Documents/rust_projects/minigrep/target/debug/minigrep.dSYM: /Users/moommen/Documents/rust_projects/minigrep/src/main.rs /Users/moommen/Documents/rust_projects/minigrep/src/lib.rs
